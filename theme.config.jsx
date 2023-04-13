6edfefca763b1db82046efd4e08c7eba51b35d02
nbryleibanez <nbryleibanez@gmail.com>
2023-04-13 23:04:56 +0800

    deployment test

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,7 +1,14 @@
 export default {
-    logo: <span>Sakyanan</span>,
+    logo: <span>Sakyanan Archive</span>,
     project: {
       link: 'https://github.com/nbryleibanez/sakyanan-archive',
     },
-    // ...
+    search: {
+      placeholder: "Search Archive",
+    },
+    footer: {
+      text: "Sakyanan Archive"
+    }
   }
+
+  
